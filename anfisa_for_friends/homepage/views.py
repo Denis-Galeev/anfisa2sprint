bfad06fb18e881729afc52a35fb3884632f7aa85
Denis Galeev <dmdenis74chel@yandex.ru>
2024-07-17 16:37:38 +0500

    4th Commit

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,17 @@
+# from django.db.models import Q
 from django.shortcuts import render
-
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    # Для переноса длинной строки замыкаем её в скобки.
+    # Будьте внимательны.
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(
+        is_published=True, is_on_main=True
+    ).order_by('title')[:3]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
